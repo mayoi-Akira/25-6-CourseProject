5c0edb41873aa281d8c2c791d887cd3be1bf3bc8
mayoi_Akira <akane36@163.com>
2025-06-19 08:57:39 +0800

    新增登录系统，使用全局变量记录当前用户

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from flask import Flask, request, jsonify
 3：用户名不合法
 4：用户已存在
 5：未获取到JSON
+6：用户不存在
+7：密码不匹配
 10：数据库错误
 '''
 
@@ -22,6 +24,8 @@ con = pymysql.connect(host='localhost',
 
 app = Flask(__name__)
 
+now_user = 0
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -30,33 +34,39 @@ def register():
     if not data:
         return jsonify({'code': 5})
 
-    user_name, password, confirm_password, role = \
-    data.get('user_name'), data.get('password'), data.get('confirm_password'), data.get('role')
+    username, password, confirm_password, role = \
+    data.get('username'), data.get('password'), data.get('confirm_password'), data.get('role')
 
     if (password != confirm_password):
         return jsonify({'code': 1})
 
     password_pattern = re.compile(r'^[\x20-\x7E]{6,20}$')
-    user_name_pattern = re.compile(r'^[A-Za-z0-9_]{6,20}$')
+    username_pattern = re.compile(r'^[A-Za-z0-9_]{6,20}$')
 
     if not bool(password_pattern.fullmatch(password)):
         return jsonify({'code': 2})
-    if not bool(user_name_pattern.fullmatch(user_name)):
+    if not bool(username_pattern.fullmatch(username)):
         return jsonify({'code': 3})
 
     cur = con.cursor()
 
+    #检查用户名是否存在
     sql = f'''
         select 1 from users
-        where name="{user_name}"
+        where username="{username}"
     '''
-    cur.execute(sql)
+    try:
+        cur.execute(sql)
+    except:
+        return jsonify({'code': 10})
+
     if cur.fetchone():
         return jsonify({'code': 4})
 
+    #加入新用户
     sql = f'''
-        insert into users(name, password, role)
-        values("{user_name}", "{password}", "{role}")
+        insert into users(username, password, role)
+        values("{username}", "{password}", "{role}")
     '''
 
     try:
@@ -66,6 +76,41 @@ def register():
     con.commit()
     cur.close()
     return jsonify({'code': 0})
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({'code': 5})
+
+    username = data.get('username')
+    password = data.get('password')
+
+    #查找该用户的密码
+    cur = con.cursor()
+    sql = f'''
+        select id, password from users
+        where username="{username}"
+    '''
+    try:
+        cur.execute(sql)
+    except:
+        return jsonify({'code': 10})
+
+    result = cur.fetchone()
+
+    #未找到
+    if not result:
+        return jsonify({'code': 6})
+
+    if result[1] != password:
+        return jsonify({'code': 7})
+
+    global now_user
+    now_user = result[0]
+    return jsonify({'code': 0, 'id': now_user})
 
 
 if __name__ == '__main__':
